241fb027cd2a284579420943ccdb31f9997ba398
Mrdjh <2351023707@qq.com>
2020-08-17 17:58:14 +0800

    更新登录注册页面，模板合并

---
+++ application/index/controller/Login.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Create By 慕白
+ * Blog：www.php1314.cn
+ * Date： 2020/8/17 15:34
+ */
---
+++ application/index/controller/Order.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Create By 慕白
+ * Blog：www.php1314.cn
+ * Date： 2020/8/13 14:53
+ */
